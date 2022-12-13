e9787740aa8d31b4ae3bada7e8464c538468000d
smriti.mool <mool.smreeti@gmail.com>
2022-12-13 09:31:52 -0500

    manage and update status in book order and order item

--- Malti_FinalProject/controllers/manageInventoryController.js
+++ Malti_FinalProject/controllers/manageInventoryController.js
@@ -16,23 +16,34 @@ const manageInventory = (req, res) => {
 
 const manageOrderItem = (req, res) => {
     let bookOrderID = req.params.bookOrderID;
-    let bookOrderQuery = `SELECT orderNumber, quantity, orderedDate 
+    let bookOrderQuery = `SELECT bookOrderID, orderNumber, quantity, orderedDate, b.orderStatusID 
                         FROM bookorder b
                         JOIN orderstatus os ON b.orderStatusID = os.orderStatusID
                         WHERE b.bookOrderID = ${bookOrderID}`;
 
+    let orderStatusQuery = `SELECT orderStatusID, code FROM orderStatus`;
     try {
-        dbConnection.query(bookOrderQuery, async (error, bookOrder) => {
-            let bookOrderItemQuery = `SELECT oi.quantity, b.name FROM orderitem oi
-                                    JOIN book b ON b.bookID = oi.bookID
-                                    WHERE oi.bookOrderID = ${bookOrderID}`;
-            try {
-                await dbConnection.query(bookOrderItemQuery, async (error, orderItems) => {
-                    let bookOrderDetails = { bookOrder: bookOrder[0], orderItems };
-                    res.render('manageOrderDetails', { bookOrderDetails });
+        dbConnection.query(orderStatusQuery, async (error, orderStatus) => {
+            if (orderStatus) {
+                dbConnection.query(bookOrderQuery, async (error, bookOrder) => {
+
+                    let bookOrderItemQuery = `SELECT oi.orderItemID, oi.quantity, b.name, os.orderStatusID, os.code 
+                                FROM orderitem oi
+                                JOIN book b ON b.bookID = oi.bookID
+                                JOIN orderstatus os ON os.orderStatusID = oi.orderStatusID
+                                WHERE oi.bookOrderID = ${bookOrderID}`;
+
+                    if (bookOrder) {
+                        dbConnection.query(bookOrderItemQuery, async (error, orderItems) => {
+                            let bookOrderDetails = { bookOrder: bookOrder[0], orderItems };
+                            res.render('manageOrderDetails', { bookOrderDetails, orderStatus });
+                        })
+                    } else {
+                        res.redirect('/manageInventory');
+                    }
                 })
-            } catch (error) {
-                console.log(error);
+            } else {
+                res.redirect('/manageInventory');
             }
         })
     } catch (error) {
@@ -41,8 +52,37 @@ const manageOrderItem = (req, res) => {
 }
 
 const verifyOrder = (req, res) => {
-    console.log(req.body);
-    
+
+    const { bookOrderDetails: bookOrderDetailsString, bookOrderStatus } = req.body;
+    let bookOrderDetails = JSON.parse(bookOrderDetailsString);
+    const { bookOrder, orderItems } = bookOrderDetails;
+    console.log(req.body)
+
+    //update each order status in order items
+    orderItems.forEach(orderItem => {
+        let updateOrderItemQuery = `UPDATE orderItem SET orderStatusID = ${orderItem.orderStatusID} 
+                                    WHERE orderItemID =${orderItem.orderItemID}`;
+        try {
+            dbConnection.query(updateOrderItemQuery, async (error, orderItems) => {
+            })
+        } catch (e) {
+            console.log(e);
+        }
+    })
+
+    //update order status of BookOrder
+    let updateBookOrderItemQuery = `UPDATE bookOrder SET orderStatusID = ${bookOrderStatus} 
+                                    WHERE bookOrderID =${bookOrder.bookOrderID}`;
+
+    try {
+        dbConnection.query(updateBookOrderItemQuery, async (error, response) => {
+            console.log(error, response);
+        })
+    } catch (e) {
+        console.log(e);
+    }
+
+    res.redirect('/manageInventory');
 }
 
 module.exports = { manageInventory, manageOrderItem, verifyOrder };
--- Malti_FinalProject/public/js/verifyOrder.js
+++ Malti_FinalProject/public/js/verifyOrder.js
@@ -1,8 +1,27 @@
 $(document).ready(() => {
 
-    $(".orderStatus").click(() => {
-        let bookOrderDetails = $('#bookOrderDetails').val();
-        alert("cds",bookOrderDetails)
+    $(".orderStatus").click((e) => {
+        let bookOrderDetails = JSON.parse($("#bookOrderDetails").val());
+        let orderItemAndOrderStatus = $(e.target).attr('state');
+        let orderItemSplit = orderItemAndOrderStatus.split('|');
+        const bookOrderItemID = orderItemSplit[0];
+        const orderItemStatus = orderItemSplit[1];
+        const orderItemStatusCode = orderItemSplit[2];
+
+        bookOrderDetails.orderItems = bookOrderDetails.orderItems.map(bookOrderItem => {
+
+            if (bookOrderItem.orderItemID == bookOrderItemID) {
+                bookOrderItem.orderStatusID = Number(orderItemStatus);
+                bookOrderItem.orderStatus = orderItemStatusCode;
+            }
+            return bookOrderItem;
+        });
+
+        $("#bookOrderDetails").val(JSON.stringify(bookOrderDetails));
+
+        console.log(bookOrderDetails)
     })
-});
+})
+
+
 
