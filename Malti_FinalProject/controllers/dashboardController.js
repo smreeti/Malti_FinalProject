1074b910b732c633098e4ef97998c36072f1f83c
smriti.mool <mool.smreeti@gmail.com>
2022-12-22 13:10:22 -0500

    add images dynamically in dashboard

--- Malti_FinalProject/controllers/dashboardController.js
+++ Malti_FinalProject/controllers/dashboardController.js
@@ -5,13 +5,15 @@
 let { dbConnection } = require("../database/dbConfig.js");
 
 const dashboard = (req, res) => {
-  let booksQuery = `SELECT b.name as bookName, CONCAT(a.firstName, ' ', a.lastName) as authorName, bc.categoryName, b.price, b.ISBN FROM book AS b
+  let booksQuery = `SELECT b.name as bookName, CONCAT(a.firstName, ' ', a.lastName) as authorName, bc.categoryName, b.price, b.ISBN, b.image
+                        FROM book AS b
                         JOIN author AS a
                             ON b.authorId = a.authorId
                         JOIN bookcategory AS bc
                             ON b.bookCategoryID = bc.bookCategoryId`;
 
-  let stockQuery = `SELECT b.name as bookName, CONCAT(a.firstName, ' ', a.lastName) as authorName, bc.categoryName, b.price, b.ISBN, s.quantity FROM book AS b
+  let stockQuery = `SELECT b.name as bookName, CONCAT(a.firstName, ' ', a.lastName) as authorName, bc.categoryName, b.price, b.ISBN, s.quantity, b.image
+                        FROM book AS b
                         JOIN bookstock AS s
                             ON b.bookID = s.bookID
                         JOIN author AS a
